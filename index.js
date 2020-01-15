7033ee7c71d3c3a8a5070eb331f93578ee8b2d57
Jason Zhang <45471919+miesoz@users.noreply.github.com>
2020-01-15 11:16:12 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,112 +1,222 @@
 // doing stuff to the document when ti loads 
+let ctx = document.getElementById('myChart').getContext('2d');
+let myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Coin in USD',
+      data: [],
+      fill: false,
+      backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+      borderColor: ['rgba(255, 99, 132, 1)' ],
+      borderWidth: 3
+    }]
+  },
+  options: {
+    responsive: true,
+    hover: {
+      mode: 'nearest',
+      intersect: false
+    },
+    scales: {
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'USD'
+        }
+      }],
+      xAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Date'
+        }
+      }]
+    },
+  }
+});
 document.addEventListener('DOMContentLoaded', () => {
-  // Call the function to get api data and populate
-  const coinID = [];
-  getCoins(coinID);
-  clicky(event, coinID)
+  let coinDict = {};
+  //let myChart;
+  build(coinDict);
+  console.log(coinDict);
+  //let xlabels = [];
+  //let ydata = [];
+  //getHistory(xlabels, ydata, "btc-bitcoin");
+  const ctx = document.getElementById('myChart').getContext('2d');
+  myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [],
+      datasets: [{
+        label: 'Coin in USD',
+        data: [],
+        fill: false,
+        backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+        borderColor: ['rgba(255, 99, 132, 1)' ],
+        borderWidth: 3
+      }]
+    },
+    options: {
+      responsive: true,
+      hover: {
+        mode: 'nearest',
+        intersect: false
+      },
+      scales: {
+        yAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'USD'
+          }
+        }],
+        xAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Date'
+          }
+        }]
+      },
+    }
+  })
+  //myChart = chartFirst(coinDict);
+  //chartFirst(myChart, coinDict);
+  //chartIt();
+
+
 })
 
-const getPrice = async (coin_id) => {
-  let coinList = document.querySelector('.coin-list');
-  let response = await fetch(`https://api.coinpaprika.com/v1/coins/${coin_id}/ohlcv/today/`);
-  let data = await response.json();
-  // console.log(data[0].close);
-  coinList.innerHTML += `<li>${data[0].close}</li>`;
-}
-
-// Print everything at the same time
-
-const getCoins = async (coinID) => {
+async function build(coinDict) {        //builds coin dictionary while simaltaneously dynamically populating the entries on the table and drop down menu.
   const response = await fetch('https://api.coinpaprika.com/v1/coins');
   const data = await response.json();
   // # = id .= class NOTHING = html tags
-  let coinList = document.querySelector('.coin-list');
-  // Grabs the ul with class of coin-list
+  let coinTickerResponse, coinTickerData;
+  const table = document.querySelector('.tableData');
+  const dropMenu = document.querySelector('.coins');
   for (let i = 0; i < 10; i++) {
-    coinID.push(data[i].id);
-    coinList.innerHTML += `<li>${data[i].name + ': ' + data[i].id}</li>`;
-    // Will be literals of string instead of variable value
-    // getPrice(data[i].id);
-
-  }
-  
-  for (let j = 0; j < 10; j++) {
-    // console.log(coinID[j]);
-    getPrice(coinID[j]);
-    // getHistory(coinID[j], "2019-11-29");
-  }
-  // console.log(coinID);
-
-}
-
-
-const getHistory = async (coin, date) => {
-  // console.log(coinArray);
-  let historyList = document.querySelector('.history-list');
-  let response = await fetch(`https://api.coinpaprika.com/v1/tickers/${coin}/historical?start=${date}&interval=1h`);
-  let data = await response.json();
-  for (let i = 0; i < data.length; i++) {
-    historyList.innerHTML += `<li>${data[i].price}</li>`;
+    coinTickerResponse = await fetch(`https://api.coinpaprika.com/v1/tickers/${data[i].id}`);
+    coinTickerData = await coinTickerResponse.json();
+    coinDict[data[i].rank] = coinTickerData;
+    table.innerHTML += `<tr><td>${coinDict[data[i].rank].name}</td>` + `<td>${coinDict[data[i].rank].quotes.USD.price}</td>` + `<td>${coinDict[data[i].rank].quotes.USD.percent_change_24h}</td>` + `<td>${coinDict[data[i].rank].quotes.USD.market_cap}</td>` + '</tr';
+    dropMenu.innerHTML += '<option value="' + coinDict[data[i].rank].id + '">' + coinDict[data[i].rank].id + '</option>';
   }
 }
 
+var plot = function(e){
+  if (e.value != "Select a Coin to Plot"){
+    chartIt(e.value);
+  }
+}
 
+//const clicky = (event, name) => {
+//  document.addEventListener("click", (event) => {
 
-// Starting from input date to Date()
-const clicky = (event, coinID) => {
-  document.addEventListener("click", (event) => {
-
-    let timeThing
+    //let timeThing;
     // console.log(event.target.previousElementSibling)
-    if (event.target.className === "date") {
+   // if (event.target.className === "date") {
       // Do some validation what if I put penis inside date input field
-      const inputDate = event.target.previousElementSibling
-      const coinOption = document.querySelector(".coins")
-      const inputCoin = coinOption.value
-      // console.log(Date())
-      timeThing = inputDate.value
+  //    const inputDate = event.target.previousElementSibling;
+  //    const coinOption = document.querySelector(".coins");
+    //  const inputCoin = coinOption.value;
+      // console.log(Date());
+     // timeThing = inputDate.value;
 
-      getHistory(inputCoin, timeThing)
-    }
-  }) 
+      //getHistory(inputCoin, timeThing);
+//    }
+//  }) 
+//}
+
+async function getHistory(xlabels, ydata, coinID) { 
+  const firstTrade = await fetch(`https://api.coinpaprika.com/v1/coins/${coinID}`)
+  const firstTradeData = await firstTrade.json();
+  const tickerFetch = await fetch(`https://api.coinpaprika.com/v1/tickers/${firstTradeData.id}/historical?start=${firstTradeData.started_at}&interval=7d`);
+  const history = await tickerFetch.json();
+  for (var date in history){
+    xlabels.push(history[date].timestamp.substring(0,10));
+    ydata.push(history[date].price);
+  }
 }
 
-// CHART STUFF IF YOU WANT OT USE CHART.JS
-// ELSE LOOK UP D3 CAUSE ITS HOT AND HARD
-// const ctx = document.getElementById('myChart').getContext('2d');
-// const myChart = new Chart(ctx, {
-//   type: 'bar',
-//   data: {
-//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//     datasets: [{
-//       label: '# of Votes',
-//       data: [12, 19, 3, 5, 2, 3],
-//       backgroundColor: [
-//         'rgba(255, 99, 132, 0.2)',
-//         'rgba(54, 162, 235, 0.2)',
-//         'rgba(255, 206, 86, 0.2)',
-//         'rgba(75, 192, 192, 0.2)',
-//         'rgba(153, 102, 255, 0.2)',
-//         'rgba(255, 159, 64, 0.2)'
-//       ],
-//       borderColor: [
-//         'rgba(255, 99, 132, 1)',
-//         'rgba(54, 162, 235, 1)',
-//         'rgba(255, 206, 86, 1)',
-//         'rgba(75, 192, 192, 1)',
-//         'rgba(153, 102, 255, 1)',
-//         'rgba(255, 159, 64, 1)'
-//       ],
-//       borderWidth: 1
-//     }]
-//   },
-//   options: {
-//     scales: {
-//       yAxes: [{
-//         ticks: {
-//           beginAtZero: true
-//         }
-//       }]
-//     }
-//   }
-// });
+//var chartFirst = async (coinDict) => {
+//  let xlabels = [];
+//  let ydata = [];
+ // await getHistory(xlabels, ydata, coinDict[1]);
+ // const ctx = document.getElementById('myChart').getContext('2d');
+  //console.log(xlabels);
+  //console.log(ydata);
+  //myChart = new Chart(ctx, {
+   // type: 'line',
+    //data: {
+     // labels: xlabels,
+      //datasets: [{
+       // label: 'USD',
+        //data: ydata,
+        //fill: true,
+       // backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+//        borderColor: ['rgba(255, 99, 132, 1)' ],
+  //      borderWidth: 3
+    //    
+      //}]
+   // },
+    //options: {
+     // responsive: true,
+     // hover: {
+      //  mode: 'nearest',
+       // intersect: false
+      //},
+      //scales: {
+       // yAxes: [{
+       //   display: true,
+        //  scaleLabel: {
+         //   display: true,
+          //  labelString: 'USD'
+          //}
+        //}],
+       // xAxes: [{
+       //   display: true,
+        //  scaleLabel: {
+         //   display: true,
+          //  labelString: 'Date'
+          //}
+        //}]
+      //},
+    //}
+  //})
+  //return myChart;
+//}
+function removeData(myChart) {
+  myChart.data.labels.pop();
+  myChart.data.datasets.forEach((dataset) => {
+    dataset.data.pop();
+  });
+  myChart.update();
+}
+
+function addData(myChart, labels, data) {
+  console.log(labels);
+  myChart.data.labels = labels;
+  //console.log(myChart.data.datasets.data);
+  myChart.data.datasets[0].data = data;
+  //myChart.data.datasets.forEach((dataset) => {
+  //  dataset.data.push(data);
+  //});
+  myChart.update();
+  console.log(myChart);
+}
+
+async function chartIt(coinID) {
+  //await resetCanvas();
+  console.log(coinID);
+  //removeData(myChart);
+  let xlabels = [];
+  let ydata = [];
+  await getHistory(xlabels, ydata, coinID);
+  //const ctx = document.getElementById('myChart').getContext('2d');
+  console.log(xlabels);
+  console.log(ydata);
+  addData(myChart, xlabels, ydata);
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
 Dynamically Populate/Render Dropdown with coins
 Graph the Data
 Organize the page/Polish the UI# cryptoFinance
+
+
+1- show tweets
+2- polish UI/pageination
+3- loading page
+4- search for coin
+5- REPRODUCE YAHOO FINANCE UI
+6- Look at examples with typeaheads
+
+
+
+
+
